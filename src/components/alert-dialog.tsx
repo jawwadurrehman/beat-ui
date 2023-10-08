651d00730f9d8adad7c4802f5a143e8a6720fb08
jawwadurrehman <91111174+jawwadurrehman@users.noreply.github.com>
2023-10-08 22:46:16 +0500

    fixes

--- src/components/alert-dialog.tsx
+++ src/components/alert-dialog.tsx
@@ -1,8 +1,8 @@
 import * as React from "react"
 import * as AlertDialogPrimitive from "@radix-ui/react-alert-dialog"
 
-import { cn } from "src/lib/utils"
-import { buttonVariants } from "src/components/button"
+import { cn } from "../lib/utils"
+import { buttonVariants } from "../components/button"
 
 const AlertDialog = AlertDialogPrimitive.Root
 
--- src/components/calendar.tsx
+++ src/components/calendar.tsx
@@ -2,8 +2,8 @@ import * as React from "react"
 import { ChevronLeft, ChevronRight } from "lucide-react"
 import { DayPicker } from "react-day-picker"
 
-import { cn } from "src/lib/utils"
-import { buttonVariants } from "src/components/button"
+import { cn } from "../lib/utils"
+import { buttonVariants } from "../components/button"
 
 export type CalendarProps = React.ComponentProps<typeof DayPicker>
 
